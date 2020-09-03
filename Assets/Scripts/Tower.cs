20abf6ffff8d2a6f8b7da3e45a5002c6543a78bb
Mike <Miket9717@gmail.com>
2020-09-03 11:43:28 +0300

    End of tutorial 1

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    public int type = 1;
+    public float AttackSpeed = 1;
+    public float Damage = 1;
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
